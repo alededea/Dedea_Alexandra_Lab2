4b39a7563e26918540b2d01321e441b041d35641
Alexandra Dedea <dedea.alexandra19@outlook.com>
2023-10-19 16:10:04 +0300

    Add project files.

---
+++ Data/Dedea_Alexandra_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dedea_Alexandra_Lab2.Models;
+
+namespace Dedea_Alexandra_Lab2.Data
+{
+    public class Dedea_Alexandra_Lab2Context : DbContext
+    {
+        public Dedea_Alexandra_Lab2Context (DbContextOptions<Dedea_Alexandra_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dedea_Alexandra_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Dedea_Alexandra_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Dedea_Alexandra_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Migrations/20231019112212_PublisherLast.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dedea_Alexandra_Lab2.Migrations
+{
+    public partial class PublisherLast : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
